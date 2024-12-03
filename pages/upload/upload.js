7c2553d82a6ace013a4b993d77f12bad8faf997f
karim2111 <rifaikm@gmail.com>
2024-12-03 00:59:52 -0500

    get most listened

--- pages/upload/upload.js
+++ pages/upload/upload.js
@@ -4,12 +4,33 @@ export function processLargeJsonFile(file) {
     reader.onload = function (event) {
       // Parse the JSON string to an object
       const jsonData = JSON.parse(event.target.result);
-      var num = 0;
+    
+      const dict = {};
+
       // Assuming jsonData is an array, loop through each item
       jsonData.forEach((item, index) => {
-        num +=1;
+        if ( jsonData[index].ms_played > 60000 ) {
+            const trackName = jsonData[index].master_metadata_track_name;
+            if (!dict[trackName]) {
+                dict[trackName] = 1;  
+            }
+            dict[trackName] += 1; 
+        }
+       
       });
-      console.log('Number of items:', num);
+     
+      console.log(Object.keys(jsonData)[0]); // num items
+      console.log(jsonData[0]); // first item
+      
+      console.log(dict); // dictionary of track names and play counts
+
+
+      const maxKey = Object.keys(dict).reduce((maxKey, currentKey) => {
+        return dict[currentKey] > dict[maxKey] ? currentKey : maxKey;
+      }, Object.keys(dict)[0]);
+      
+      console.log(maxKey, dict[maxKey]); 
+
     };
     
     reader.readAsText(file);
